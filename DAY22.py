a5291d342df071f5675686b44c8931f5a1329cba
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2023-03-14 16:50:10 +0300

    Add files via upload

---
+++ DAY22.py
@@ -0,0 +1,35 @@
+#https://www.hackerrank.com/challenges/30-binary-search-trees/
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def getHeight(self,root):
+        if root is None:
+            return -1
+        else:
+            left_height = self.getHeight(root.left)
+            right_height = self.getHeight(root.right)
+        
+        return 1 + max(left_height, right_height)
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+height=myTree.getHeight(root)
+print(height)       
